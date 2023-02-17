523b4ffd72fcb63ed2e7acefcc41f293da726a84
benadidafouzia <benadidafouzia@gmail.com>
2023-02-17 14:05:11 +0300

    right sidebar completed

--- src/components/RightSideBar.vue
+++ src/components/RightSideBar.vue
@@ -17,6 +17,136 @@
         <v-icon x-small color="#7C7C7F">fas fa-chevron-down</v-icon>
       </v-btn>
     </v-toolbar>
+    <!-- <h1>ici doit etre dynamique</h1> -->
+    <strong class="ml-3 white--text">Overall Progress</strong> <br />
+    <strong class="ml-3 grey--text caption">Suziki Frontend</strong>
+    <div class="text-center mb-5 mt-n5">
+      <v-progress-circular
+        :size="100"
+        :width="12"
+        :value="70"
+        color="orange"
+        class="mt-5"
+      >
+        70%</v-progress-circular
+      >
+    </div>
+    <strong class="ml-3 white--text">Recent Activities</strong>
+    <strong class="ml-3 grey--text caption">{{ currentDate() }}</strong>
+    <v-list three-line>
+      <v-list-item-group>
+        <template v-for="item in files">
+          <v-list-item :key="item.title">
+            <v-list-item-avatar>
+              <img :src="item.avatar" />
+            </v-list-item-avatar>
+            <template>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-html="item.title"
+                  class="white--text"
+                ></v-list-item-title>
+                <v-list-item-subtitle
+                  v-text="item.subtitle"
+                  class="grey--text"
+                ></v-list-item-subtitle>
+                <v-list-item-subtitle
+                  v-text="item.time"
+                  class="grey--text"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-card
+          class="d-flex align-center ml-4"
+          height="60px"
+          width="120px"
+          color="#21212B"
+        >
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item two-line class="text-center">
+                <v-list-item-content>
+                  <v-list-item-title class="white--text">550</v-list-item-title>
+                  <v-list-item-subtitle class="grey--text caption mt-2"
+                    >Total Project</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card
+          class="d-flex align-center mx-2"
+          height="60px"
+          width="120px"
+          color="#21212B"
+        >
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item two-line class="text-center">
+                <v-list-item-content>
+                  <v-list-item-title class="white--text">200</v-list-item-title>
+                  <v-list-item-subtitle class="grey--text caption mt-2"
+                    >Upcoming</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card
+          class="d-flex align-center ml-4"
+          height="60px"
+          width="120px"
+          color="#21212B"
+        >
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item two-line class="text-center">
+                <v-list-item-content>
+                  <v-list-item-title class="white--text">320</v-list-item-title>
+                  <v-list-item-subtitle class="grey--text caption mt-2"
+                    >In Progress</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card
+          class="d-flex align-center ml-4"
+          height="60px"
+          width="120px"
+          color="#21212B"
+        >
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item two-line class="text-center">
+                <v-list-item-content>
+                  <v-list-item-title class="white--text">129</v-list-item-title>
+                  <v-list-item-subtitle class="grey--text caption mt-2"
+                    >Completed</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-navigation-drawer>
 </template>
 
@@ -24,9 +154,48 @@
 export default {
   name: "RightSideBar",
   data() {
-    return {};
+    return {
+      files: [
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+          time: "5 Mins Ago",
+          subtitle: `Backend Developer`,
+          title: `<strong>Mike Loke</strong>`,
+        },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+          time: "15 Mins Ago",
+          subtitle: `Senior Quality Assurance`,
+          title: `<strong>Sarah Hosten</strong>`,
+        },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+          time: "30 Mins Ago",
+          subtitle: `Business Development`,
+          title: `<strong>Dena Thompson</strong>`,
+        },
+      ],
+    };
+  },
+  methods: {
+    currentDate() {
+      const current = new Date();
+      const date = `${current.getDate()}/${
+        current.getMonth() + 1
+      }/${current.getFullYear()}`;
+      return date;
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.v-card.borderme {
+    border: 2px solid #704232 !important;
+}
+ .col-12 {
+    padding: 5px !important;
+}
+.v-btn.withoutupercase{
+    text-transform: none !important;
+}</style>
